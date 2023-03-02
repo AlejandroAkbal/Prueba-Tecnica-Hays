4d7facb3b0e1a9c74740b6e72e0b0f45a3618bb2
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-03-02 17:55:18 +0100

    feat: add OpenAPI annotations

--- app/Http/Controllers/PostApiController.php
+++ app/Http/Controllers/PostApiController.php
@@ -6,9 +6,26 @@ use App\Http\Requests\PostStoreRequest;
 use App\Http\Resources\PostResource;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Http;
+use OpenApi\Annotations as OA;
 
 class PostApiController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/api/posts",
+     *     tags={"Posts"},
+     *     summary="Get all posts",
+     *     description="Get all posts",
+     *     operationId="posts.index",
+     *     @OA\Response(
+     *     response=200,
+     *     description="Success",
+     *     @OA\JsonContent(
+     *     type="array",
+     *     @OA\Items(ref="#/components/schemas/PostResource")
+     *    )
+     *   ),
+     */
     public function index(): JsonResponse
     {
         $postsResponse = Http::get('https://jsonplaceholder.typicode.com/posts');
@@ -18,6 +35,28 @@ class PostApiController extends Controller
         return $postsResource->response();
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/posts",
+     *     tags={"Posts"},
+     *     summary="Create a post",
+     *     description="Create a post",
+     *     operationId="posts.store",
+     *     @OA\RequestBody(
+     *     required=true,
+     *     @OA\JsonContent(ref="#/components/schemas/PostStoreRequest")
+     *    ),
+     *     @OA\Response(
+     *     response=201,
+     *     description="Success",
+     *     @OA\JsonContent(ref="#/components/schemas/PostResource")
+     *    ),
+     *     @OA\Response(
+     *     response=422,
+     *     description="Validation Error",
+     *     @OA\JsonContent(ref="#/components/schemas/ValidationErrorResponse")
+     *    ),
+     */
     public function store(PostStoreRequest $request): JsonResponse
     {
         $requestData = $request->validated();
@@ -33,6 +72,34 @@ class PostApiController extends Controller
         return $postResource->response()->setStatusCode(201);
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/posts/{id}",
+     *     tags={"Posts"},
+     *     summary="Get a post",
+     *     description="Get a post",
+     *     operationId="posts.show",
+     *     @OA\Parameter(
+     *     name="id",
+     *     in="path",
+     *     description="Post ID",
+     *     required=true,
+     *     @OA\Schema(
+     *     type="integer",
+     *     format="int64"
+     *    )
+     *   ),
+     *     @OA\Response(
+     *     response=200,
+     *     description="Success",
+     *     @OA\JsonContent(ref="#/components/schemas/PostResource")
+     *    ),
+     *     @OA\Response(
+     *     response=404,
+     *     description="Not Found",
+     *     @OA\JsonContent(ref="#/components/schemas/NotFoundResponse")
+     *    ),
+     */
     public function show(int $id): JsonResponse
     {
         $postResponse = Http::get('https://jsonplaceholder.typicode.com/posts/' . $id);
